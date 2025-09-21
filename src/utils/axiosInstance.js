c4b224e39122873cc1abcc4e6a0856b41ad3021b
NicolasCamano <nikolas.camano@gmail.com>
2025-09-21 02:14:33 +0000

    FIX: Actualizar URL de la API para producción

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 import dayjs from 'dayjs';
 
-const baseURL = 'http://127.0.0.1:8000/api/v1';
+const baseURL = 'https://arcade-api-i5r3.onrender.com/api/v1';
 
 const axiosInstance = axios.create({
     baseURL
